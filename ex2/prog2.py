485ad7fca8a48406ce169d7b961fa0b23dc1f8fd
HannanehGhanbarnejad <79471968+HannanehGhanbarnejad@users.noreply.github.com>
2021-05-02 05:36:34 -0700

    Update prog2.py

--- ex2/prog2.py
+++ ex2/prog2.py
@@ -8,23 +8,19 @@ def prog2(a, b):
     bigger than the second one numerically .
 
     >>> prog2(23, 3) #23 as a, 3 as b
-    Even numbers between 3 and 23 are: 
     [4, 6, 8, 10, 12, 14, 16, 18, 20, 22]
     
-    >>> prog2(56, 67) #56 as a, 67 as b
-    The even numbers between 56 and 67 are: 
+    >>> prog2(56, 67) #56 as a, 67 as b 
     [56, 58, 60, 62, 64, 66]
 
      """
     if b>a:
-        print("The even numbers between", a,"and", b ,"are: ")
         L=[]
         for i in range(a,b+1):
             if i%2 == 0:
                 L.append(i)
         print(L)
     else:
-        print("The even numbers between", b,"and", a ,"are: ") 
         L1=[]
         for j in range(b,a+1):
              if j%2 == 0:
